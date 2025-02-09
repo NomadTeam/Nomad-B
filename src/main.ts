63df100226b3975b7395340c90c7f72baf5e5862
ssssu <cdw3521@naver.com>
2025-02-09 15:21:19 +0900

    🔥 Remove

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import { HttpExceptionFilter } from '@common/filters/http-exception-filter';
-import { InternalServerError } from '@common/filters/server-error-exception';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  app.useGlobalFilters(new HttpExceptionFilter(), new InternalServerError());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
