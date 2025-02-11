a79ced381a0f0d8d658542dc2d3ece3ebaf12b4c
ssssu <cdw3521@naver.com>
2025-02-11 17:34:50 +0900

    🤡 docs: mock 데이터 파일 추가

---
+++ src/destination/mocks/mock-data.ts
@@ -0,0 +1,57 @@
+export const mockDestination = [
+  {
+    id: '12345',
+    name: '여행지1',
+    address: '주소1',
+    information: '설명',
+    latitude: 37.123456,
+    longitude: 127.123456,
+    category: '카테고리1',
+  },
+  {
+    id: '12346',
+    name: '여행지2',
+    address: '주소2',
+    information: '설명',
+    latitude: 37.123457,
+    longitude: 127.123457,
+    category: '카테고리1',
+  },
+  {
+    id: '12347',
+    name: '여행지3',
+    address: '주소3',
+    information: '설명',
+    latitude: 37.123458,
+    longitude: 127.123458,
+    category: '카테고리2',
+  },
+  {
+    id: '12348',
+    name: '여행지4',
+    address: '주소4',
+    information: '설명',
+    latitude: 37.123459,
+    longitude: 127.123459,
+    category: '카테고리2',
+  },
+  {
+    id: '12349',
+    name: '여행지5',
+    address: '주소5',
+    information: '설명',
+    latitude: 37.123452,
+    longitude: 127.123452,
+    category: '카테고리3',
+  },
+];
+
+export const mockImage = [{ image: 'image' }];
+export const mockRecomm = [{ count: 1 }];
+export const mockErrDestination = [
+  { id: '23456', name: '식당1' },
+  { id: '34567', name: '카페1' },
+];
+
+export const mockErrStr: unknown = 'test';
+export const mockErrArr = [[null], []];
