6719f8762f7e739ca68cd2577ccffa97db14cd2e
ssssu <cdw3521@naver.com>
2025-02-12 15:04:29 +0900

    🐛 fix: validateData에서 에러가 발생해도 추천 클릭 수가 db에 저장되는 에러 해결 - 트랜잭션 코드 추가(원자성 적용) 👔 docs: 해당 여행지의 추천도 반환하는 로직 추가

--- src/recommendation/recommendation.service.ts
+++ src/recommendation/recommendation.service.ts
@@ -6,13 +6,19 @@ import {
 import { RecommendationRepository } from './recommendation.repository';
 import { DestinationRepository } from '@destination/destination.repository';
 import { error } from 'console';
+import * as mysql from 'mysql2/promise';
+import { ConnectRepository } from '@data/data.repository';
 
 @Injectable()
 export class RecommendationService {
+  private readonly pool: mysql.Pool;
   constructor(
     private recommDB: RecommendationRepository,
     private destDB: DestinationRepository,
-  ) {}
+    private connectDB: ConnectRepository,
+  ) {
+    this.pool = this.connectDB.getPool();
+  }
 
   /**
    * @param email 추천 버튼을 누른 유저 이메일
@@ -35,18 +41,25 @@ export class RecommendationService {
   /**
    * @param email 추천 버튼을 누른 유저 이메일
    * @param id 유저가 추천하고자 하는 여행지 아이디
-   * @returns 성공 메시지
+   * @returns 성공 메시지 및 추천도
    */
   async pushRecommendation(email: string, id: string) {
+    const connection = await this.pool.getConnection();
     try {
-      await Promise.all([
-        this.validateData(email, id),
-        this.recommDB.addRecommendation(email, id),
-      ]);
+      await connection.beginTransaction(); // 트랜잭션 시작
 
-      return { message: '추천 완료' };
+      await this.validateData(email, id);
+
+      await this.recommDB.addRecommendation(email, id);
+      await connection.commit(); // 트랜잭션 커밋
+
+      const result = await this.destDB.getRecommByDestId(id);
+      return { recomm: result[0].count, message: '추천 완료' };
     } catch (e) {
+      await connection.rollback(); // 에러 발생 시 트랜잭션 롤백
       throw e;
+    } finally {
+      connection.release(); // 커넥션 반환
     }
   }
 }
