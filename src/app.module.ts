e2abc2f80e6e71fc529810a1fcfae15457640c02
ssssu <cdw3521@naver.com>
2025-02-13 15:47:30 +0900

    🔧 config: jwt strategy, jwt guard 전역 해제

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,36 +4,28 @@ import { AppService } from '@src/app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DataModule } from '@data/data.module';
 import { UsersModule } from '@users/users.module';
+import { CategoriesModule } from '@categories/categories.module';
+import { DestinationModule } from '@destination/destination.module';
+import { RecommendationModule } from '@recommendation/recommendation.module';
 import { JwtModule } from '@nestjs/jwt';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtGuard } from '@users/jwt/jwt.guard';
-import { CategoriesModule } from './categories/categories.module';
-import { DestinationModule } from './destination/destination.module';
-import { RecommendationModule } from './recommendation/recommendation.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 전역 모듈로 선언
     }),
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '1h' },
-    }),
     DataModule,
     UsersModule,
     CategoriesModule,
     DestinationModule,
     RecommendationModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
