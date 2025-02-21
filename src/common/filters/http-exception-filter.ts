0b2a9d231594dce25cdd8990823dec1771f4b1f6
ssssu <cdw3521@naver.com>
2025-02-21 16:26:13 +0900

    ♻️ refactor: error interface 정의 및 예외처리 응답 코드 수정

--- src/common/filters/http-exception-filter.ts
+++ src/common/filters/http-exception-filter.ts
@@ -7,6 +7,12 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 
+interface ErrorResponse {
+  error: string;
+  message: string | string[];
+  statusCode: number;
+}
+
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(HttpException.name);
@@ -14,38 +20,20 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
-    const error = exception.getResponse() as
-      | string
-      | { error: string; statusCode: number; message: string | string[] };
+    const error = exception.getResponse() as ErrorResponse;
 
-    if (status === 500) {
-      this.logger.log(error);
+    if (status === 500) this.logger.error(error);
 
-      response.status(500).json({
-        err: '서버 오류입니다. 잠시 후 다시 이용해주세요.',
-        data: null,
-      });
-    }
-
-    if (typeof error === 'string') {
+    if (typeof error.message === 'string') {
       response.status(status).json({
-        err: error,
-        data: null,
+        err: error.error,
+        data: error.message,
       });
-    }
-
-    if (typeof error === 'object') {
-      if (typeof error.message === 'string') {
-        response.status(status).json({
-          err: error.message,
-          data: null,
-        });
-      } else {
-        response.status(status).json({
-          err: error.message[0],
-          data: null,
-        });
-      }
+    } else {
+      response.status(status).json({
+        err: error.error,
+        data: error.message[0],
+      });
     }
   }
 }
